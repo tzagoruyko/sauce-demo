87ab1169e39096953073a18bdec98b37b0e8c872
Татьяна <ztatyana1995@yandex.ru>
2026-02-11 17:06:59 +0300

    написала негавный тест и добавила маркировку negative

--- pages/login_page.py
+++ pages/login_page.py
@@ -6,9 +6,13 @@ class LoginPage(BasePage):
     USERNAME_INPUT = (By.XPATH, "//input[@id='user-name']")
     PASSWORD_INPUT = (By.XPATH, "//input[@id='password']")
     LOGIN_BUTTON = (By.XPATH, "//input[@id='login-button']")
+    ERROR_MESSAGE = (By.XPATH, "//*[@class='error-message-container error']")
 
     def login(self, username, password):
         self.send_keys(self.USERNAME_INPUT, username)
         self.send_keys(self.PASSWORD_INPUT, password)
         self.click(self.LOGIN_BUTTON)
+
+    def error_message_is_displayed(self):
+        return self.is_displayed(self.ERROR_MESSAGE)
 
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,3 +1,4 @@
+import pytest
 from pages.login_page import LoginPage
 from pages.inventory_page import InventoryPage
 from config.config import STANDARD_USER, PASSWORD
@@ -9,6 +10,15 @@ def test_success_login(driver):
 
     inventory_page = InventoryPage(driver)
     assert inventory_page.get_current_url() == inventory_page.URL, "Должна была открыться страница каталога"
+
+@pytest.mark.negative
+def test_incorrect_password(driver):
+    login_page = LoginPage(driver)
+    login_page.open_page(login_page.URL)
+    login_page.login(STANDARD_USER, "incorrect password")
+
+    assert login_page.get_current_url() == login_page.URL, "Должна была остаться открытой страница логина"
+    assert login_page.error_message_is_displayed(), "Должна выводиться ошибка логина"
 
 
 
